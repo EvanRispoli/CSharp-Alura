648d655d29d349de0bdaeaf2ba9328d167e62673
Evan Rispoli <evan.rispoli@gmail.com>
2022-03-23 12:45:35 -0300

    Primeiro Prgrama

--- 1-PrimeiroPrograma/Program.cs
+++ 1-PrimeiroPrograma/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, mundo. Projeto no Visual Studio!");
+
+            Console.WriteLine("A execução acabou. Tecle enter para finalizar. . . ");
+            Console.ReadLine();
+        }
+    }
+}
