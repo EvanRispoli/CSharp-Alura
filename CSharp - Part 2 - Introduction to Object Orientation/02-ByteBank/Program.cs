57c946370a89ddd6f2593097030b984b51465986
Evan Rispoli <evan.rispoli@gmail.com>
2022-03-27 19:28:53 -0300

    Valores padrao

---
+++ CSharp - Part 2 - Introduction to Object Orientation/02-ByteBank/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+ContaCorrente conta = new ContaCorrente();
+
+conta.titular = "Evan";
+Console.WriteLine("Saldo: " + conta.saldo);
+
+conta.saldo = 200;
+
+Console.WriteLine(conta.titular);
+Console.WriteLine("Agência: " + conta.agencia);
+Console.WriteLine("Saldo: " + conta.saldo);
+
+Console.ReadLine();
